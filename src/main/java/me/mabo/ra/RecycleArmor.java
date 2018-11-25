0d91f32f644ce6653a8c6a1bd800d5315285df70
marcoboekholt <dhrmabo@gmail.com>
2018-11-25 17:56:05 +0100

    Added update checker

--- src/main/java/me/mabo/ra/RecycleArmor.java
+++ src/main/java/me/mabo/ra/RecycleArmor.java
@@ -5,7 +5,6 @@ import me.mabo.ra.proxy.CommonProxy;
 import me.mabo.ra.util.Reference;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -15,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import java.util.logging.Logger;
 
 
-@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, updateJSON = Reference.UPDATE_URL)
 public class RecycleArmor {
 
     @Mod.Instance
@@ -28,11 +27,13 @@ public class RecycleArmor {
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent e) {
+        proxy.preInit();
         logger = Logger.getLogger(Reference.MOD_ID);
     }
 
     @Mod.EventHandler
     public static void init(FMLInitializationEvent e) {
+        proxy.init();
         RecipesInit.removeRecipes(new ItemStack(Items.IRON_NUGGET), Reference.MOD_ID);
         RecipesInit.removeRecipes(new ItemStack(Items.GOLD_NUGGET), Reference.MOD_ID);
         RecipesInit.init();
@@ -41,7 +42,7 @@ public class RecycleArmor {
 
     @Mod.EventHandler
     public static void postInit(FMLPostInitializationEvent e) {
-
+        proxy.postInit();
     }
 
 
--- src/main/java/me/mabo/ra/util/Reference.java
+++ src/main/java/me/mabo/ra/util/Reference.java
@@ -3,8 +3,9 @@ package me.mabo.ra.util;
 public class Reference {
     public static final String MOD_ID = "ra";
     public static final String NAME = "Recycle Armor";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.12.2-1.0.0";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
     public static final String CLIENT = "me.mabo.ra.proxy.ClientProxy";
     public static final String COMMON = "me.mabo.ra.proxy.CommonProxy";
+    public static final String UPDATE_URL = "https://raw.githubusercontent.com/MarcoBoekholt/Recycle-Armor/1.12.x/update.json";
 }
